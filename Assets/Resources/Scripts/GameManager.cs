0690636abb15d184835d28e7ab9617baecf25582
Joshitha L. Muthukrishnan <jlmuthuk@ncsu.edu>
2021-01-30 23:24:47 -0500

    Initializing dev branch (work off of this one)

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Singleton class that handles game state (keeps track of progress)
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameManager>().Length;
+
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ResetGameStatus()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Resources/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // namespace containing functionality for scene management
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    public void LoadStartScene() {
+        FindObjectOfType<GameManager>().ResetGameStatus();
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
